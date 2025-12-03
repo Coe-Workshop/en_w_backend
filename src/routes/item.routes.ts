4a2472066654246a57c5ccef685e0cf60c6f22bd
Insee <insee134@gmail.com>
2025-12-03 22:34:31 +0700

    CW-2 delete unrelated file.

--- src/routes/item.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getAllItems } from "../controllers/itemsController";
-const router = Router();
-
-router.get("/", getAllItems);
-// router.post("/item");
-
-export default router;
