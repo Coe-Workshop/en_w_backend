0a523396ec9c727a3478677130be3e4febb57703
Insee <insee134@gmail.com>
2026-02-01 17:48:05 +0700

    feat: fix cors

--- src/internal/server/server.ts
+++ src/internal/server/server.ts
@@ -10,7 +10,7 @@ const makeServer = () => {
 
   app.use(
     cors({
-      origin: process.env.FRONTEND_URL || "http://localhost:3000",
+      origin: [process.env.FRONTEND_URL || "https://dev-coe.ionize13.com", "http://localhost:3000"],
       credentials: true,
     }),
   );
