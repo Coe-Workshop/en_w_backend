62ccde6e263dbc0691a42efdf380d03bda519a36
Insee <insee134@gmail.com>
2026-02-01 18:39:57 +0700

    feat: fix cors

--- src/internal/server/server.ts
+++ src/internal/server/server.ts
@@ -8,13 +8,22 @@ import { db } from "@/config/drizzle";
 const makeServer = () => {
   const app = express();
 
-  app.use(
-    cors({
-      origin: [process.env.FRONTEND_URL || "https://dev-coe.ionize13.com", "http://localhost:3000"],
-      credentials: true,
-    }),
-  );
+  const allowedOrigins = [process.env.FRONTEND_URL , "http://localhost:3000"]
 
+  const corsOptions = {  
+    origin: (origin: string | undefined, callback: Function) => {  
+      if (!origin) return callback(null, true);  
+
+      if (allowedOrigins.includes(origin)) {  
+	callback(null, true);
+      } else {  
+	callback(new Error('Not allowed by CORS')); 
+      }  
+    },  
+    credentials: true,
+  };
+
+  app.use(cors(corsOptions));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
